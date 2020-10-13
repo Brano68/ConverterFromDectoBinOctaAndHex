31a37bfaf10a60db937a6c31050c62c091a9eb95
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2020-10-13 16:14:08 +0200

    added binToDec OctToDec and HexToDec

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main {
 
             Scanner sc = new Scanner(System.in);
             int volba, cislo;
+            String retazec;
 
     do{
                 System.out.println("-----PREVOD SUSTAV-----");
@@ -14,21 +15,47 @@ public class Main {
                 System.out.println("1: Prevod do dvojkovej");
                 System.out.println("2: Prevod do osmickovej");
                 System.out.println("3: Prevod do sesnastkovej");
+                System.out.println("4: Prevod z dvojkovej do desiatkovej");
+                System.out.println("5: Prevod z osmickovej do desiatkovej");
+                System.out.println("6: Prevod zo sesnastkovej do desiatkovej");
+                System.out.println(("7: Nekonecny cyklus na vypocet suctu"));
                 volba = sc.nextInt();
-            }while(volba<1 || volba>3);
-
-    System.out.println("Zadaj cislo ktore chces previst: ");
-    cislo = sc.nextInt();
+            }while(volba<1 || volba>7);
 
     switch (volba){
         case (1):
+            System.out.println("Zadaj cislo ktore chces previest: ");
+            cislo = sc.nextInt();
             convertToBinary(cislo);
             break;
         case (2):
+            System.out.println("Zadaj cislo ktore chces previest: ");
+            cislo = sc.nextInt();
             convertToOcta(cislo);
             break;
         case (3):
+            System.out.println("Zadaj cislo ktore chces previest: ");
+            cislo = sc.nextInt();
             convertToHexa(cislo);
+            break;
+        case (4):
+            System.out.println("Zadaj cislo ktore chces previest: ");
+            cislo = sc.nextInt();
+            convertFromBinToDec(cislo);
+            break;
+        case (5):
+            System.out.println("Zadaj cislo ktore chces previest: ");
+            cislo = sc.nextInt();
+            convertFromOctToDec(cislo);
+            break;
+        case (6):
+            System.out.println("Zadaj cislo ktore chces previest: ");
+            Scanner in = new Scanner(System.in);
+            retazec = in.nextLine();
+            convertFromHexToDec(retazec);
+            break;
+        case (7):
+            countInfinity();
             break;
     }
         }
@@ -134,6 +161,123 @@ public class Main {
                     System.out.print(pole[j]);
             }
         }
+    }
+
+    private static void convertFromBinToDec(int cislo){
+        int dlzka = 0;
+        int pomocneCislo = cislo;
+        int vysledok = 0;
+        //zisti velkost pola
+        while(pomocneCislo != 0){
+            pomocneCislo = pomocneCislo/10;
+            dlzka++;
+        }
+        int[] pole = new int[dlzka];
+        //ulozi do pola cisla na kazdy index jedno cislo
+        for(int i = 0; i < dlzka; i++){
+            pole[i] = cislo%10;
+            cislo = cislo/10;
+        }
+        //od zadu pola robim mocniny
+        for(int i = dlzka-1; i >= 0; i--){
+            vysledok = vysledok + pole[i]*(int)Math.pow(2,i);
+        }
+        System.out.println("Hodnota je: " + vysledok);
+    }
+
+    private static void convertFromOctToDec(int cislo){
+        int dlzka = 0;
+        int pomocneCislo = cislo;
+        int vysledok = 0;
+        //zisti velkost pola
+        while(pomocneCislo != 0){
+            pomocneCislo = pomocneCislo/10;
+            dlzka++;
+        }
+        int[] pole = new int[dlzka];
+        //ulozi do pola cisla na kazdy index jedno cislo
+        for(int i = 0; i < dlzka; i++){
+            pole[i] = cislo%10;
+            cislo = cislo/10;
+        }
+        //od zadu pola robim mocniny
+        for(int i = dlzka-1; i >= 0; i--){
+            vysledok = vysledok + pole[i]*(int)Math.pow(8,i);
+        }
+        System.out.println("Hodnota je: " + vysledok);
+    }
+
+    private static void convertFromHexToDec(String retazec){
+    //System.out.println("Testovanie " + retazec);
+    int dlzka = retazec.length();
+    int vysledok = 0;
+    int j = 0;
+
+        for(int i = dlzka-1; i >=0; i--){
+            if(retazec.charAt(i) == '0'){
+                vysledok = vysledok + 0*(int)Math.pow(16,j);
+            }
+            else if(retazec.charAt(i) == '1'){
+                vysledok = vysledok + 1*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == '2'){
+                vysledok = vysledok + 2*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == '3'){
+                vysledok = vysledok + 3*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == '4'){
+                vysledok = vysledok + 4*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == '5'){
+                vysledok = vysledok + 5*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == '6'){
+                vysledok = vysledok + 6*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == '7'){
+                vysledok = vysledok + 7*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == '8'){
+                vysledok = vysledok + 8*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == '9'){
+                vysledok = vysledok + 9*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == 'A'){
+                vysledok = vysledok + 10*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == 'B'){
+                vysledok = vysledok + 11*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == 'C'){
+                vysledok = vysledok + 12*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == 'D'){
+                vysledok = vysledok + 13*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == 'E'){
+                vysledok = vysledok + 14*(int)Math.pow(16,j);
+            }else if(retazec.charAt(i) == 'F'){
+                vysledok = vysledok + 15*(int)Math.pow(16,j);
+            }
+            j++;
+        }
+        System.out.println("Hodnota je: " + vysledok);
+    }
+
+    private static void countInfinity(){
+        Scanner sc = new Scanner(System.in);
+        int cislo;
+        int sucet = 0;
+        int pamataPredchadzajuce = 0;
+        while (true){
+            System.out.println("Zadaj cislo: ");
+            cislo = sc.nextInt();
+            if(cislo != 0 && cislo != pamataPredchadzajuce){
+                sucet = sucet + cislo;
+            }else{
+                break;
+            }
+            pamataPredchadzajuce = cislo;
+            System.out.println("Zadaj cislo: ");
+            cislo = sc.nextInt();
+            if(cislo != pamataPredchadzajuce && cislo != 0){
+                sucet = sucet + cislo;
+            }else{
+                break;
+            }
+            pamataPredchadzajuce = cislo;
+        }
+        System.out.println("Zadal si 0 alebo dve hodnoty rovnake po sebe");
+        System.out.println("Sucet cisel je: " + sucet);
     }
 
     }
